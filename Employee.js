3f3f2bff550f41d392ee04981ab0e80101fd11f5
William Grundler <wrgrundler@gmail.com>
2020-05-20 08:43:57 -0700

    classes and subclasses created

--- Employee.js
+++ Employee.js
@@ -1,1 +1,18 @@
 // TODO: Write code to define and export the Employee class
+class Employee{
+    constructor(name,id,email){
+        if (!name) {
+            throw new Error("You are missing the name.");
+        }
+        if (!id) {
+            throw new Error("You are missing the id.");
+        }
+        if (!email) {
+            throw new Error("You are missing the email.");
+        }
+        this.name = name;
+        this.id = id;
+        this.email = email;
+
+    }  
+}
--- Manager.js
+++ Manager.js
@@ -1,1 +1,18 @@
 // TODO: Write code to define and export the Manager class. HINT: This class should inherit from Employee.
+const Employee = require("./Employee.js");
+
+    class Manager extends Employee {
+        constructor(officeNumber){
+        if (!officeNumber) {
+            throw new Error("You are missing the school name.");
+        }
+        this.officeNumber = officeNumber;
+        super(name,id,email);
+    }
+
+    getRole(){
+
+    };
+}
+
+module.exports = Manager;
